048a22ea21fb97c867d843deb593553b1e787f64
jezinka <jezina1989@gmail.com>
2022-02-07 17:32:58 +0100

    zwrot środków #2

--- read_emails.py
+++ read_emails.py
@@ -57,26 +57,41 @@ def read_message(service, mail):
 
 def process_message(payload):
     headers = payload.get("headers")
+    body = prepare_body(payload)
+    income = is_income(body, headers)
 
-    income = 'Wpływ' in list(filter(lambda x: x.get("name").lower() == "subject", headers))[0].get("value")
     receive_date = list(filter(lambda x: x.get("name").lower() == "date", headers))[0].get("value")
-
-    body = urlsafe_b64decode(payload.get("body")['data']).decode("utf-8", "replace")
-    body = re.sub(r'<.*>', '', body)
-    body = re.sub(r'\s{2}', '', body)
 
     if income:
         m = re.search(r"Tytuł:(?P<tytul>[\w].*)Nadawca:(?P<kto>[\w].*)Kwota:(?P<kwota>[\d ]*,[\d]{2}) PLN", body)
+        if m is None:
+            m = re.search(
+                r"Ile:\+?(?P<kwota>[\d ]*,[\d]{2}) PLNKiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})Gdzie:(?P<tytul>[\w].*)Telefon",
+                body)
     else:
         m = re.search(
             r"(Odbiorca:(?P<kto>.*))?Ile:(?P<kwota>[\d ]*,[\d]{2}) PLNTytuł:(?P<tytul>[\w].*)Kiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})",
             body)
         if m is None:
             m = re.search(
-                r"Ile:[-+]?(?P<kwota>[\d ]*,[\d]{2}) PLNKiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})Gdzie:(?P<tytul>[\w].*)Telefon",
+                r"Ile:-?(?P<kwota>[\d ]*,[\d]{2}) PLNKiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})Gdzie:(?P<tytul>[\w].*)Telefon",
                 body)
 
     message = Message(m.groupdict(), income)
     logging.info(message.get_title())
     message.set_receive_date(receive_date)
     return message
+
+
+def is_income(body, headers):
+    income = 'Wpływ' in list(filter(lambda x: x.get("name").lower() == "subject", headers))[0].get("value")
+    if not income:
+        income = re.search(r"Ile:\+?(?P<kwota>[\d ]*,[\d]{2})", body) is not None
+    return income
+
+
+def prepare_body(payload):
+    body = urlsafe_b64decode(payload.get("body")['data']).decode("utf-8", "replace")
+    body = re.sub(r'<.*>', '', body)
+    body = re.sub(r'\s{2}', '', body)
+    return body
