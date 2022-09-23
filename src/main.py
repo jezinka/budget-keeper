713671008b38d1f719341a32be2cc7f456c3bb09
jezinka <jezina1989@gmail.com>
2022-09-23 13:24:10 +0200

    optimization

--- src/main.py
+++ src/main.py
@@ -13,38 +13,38 @@ def main():
     logging_config()
     gmail_service = get_bank_gmail_service()
     spread_service = get_gspread_service()
+    db_utils = DbUtils()
 
-    results = search_bank_messages(gmail_service, LABEL_ID)
+    while True:
+        results = search_bank_messages(gmail_service, LABEL_ID)
 
-    logging.info(f'found {len(results)} mails for processing')
-    if len(results) > 0:
-        db_utils = DbUtils()
-        for msg in results:
-            logging.info(f'message {msg[ID]} processing start')
-            try:
-                message = read_message(gmail_service, msg)
-                logging.debug(f'message {msg[ID]} read')
+        logging.info(f'found {len(results)} mails for processing')
+        if len(results) > 0:
+            for msg in results:
+                logging.info(f'message {msg[ID]} processing start')
+                try:
+                    message = read_message(gmail_service, msg)
+                    logging.debug(f'message {msg[ID]} read')
 
-                write_messages(spread_service, message)
-                logging.debug(f'message {msg[ID]} saved in sheet')
+                    write_messages(spread_service, message)
+                    logging.debug(f'message {msg[ID]} saved in sheet')
 
-                db_utils.insert_transaction(message)
-                logging.debug(f'message {msg[ID]} saved in db')
+                    db_utils.insert_transaction(message)
+                    logging.debug(f'message {msg[ID]} saved in db')
 
-                gmail_service.users().messages().modify(userId=ME_ID,
-                                                        id=msg[ID],
-                                                        body={'removeLabelIds': [LABEL_ID]}
-                                                        ).execute()
-                logging.debug(f'message {msg[ID]} label removed')
+                    gmail_service.users().messages().modify(userId=ME_ID,
+                                                            id=msg[ID],
+                                                            body={'removeLabelIds': [LABEL_ID]}
+                                                            ).execute()
+                    logging.debug(f'message {msg[ID]} label removed')
 
-            except Exception as err:
-                logging.error(f"Unexpected error: {msg[ID]}: {err}")
-                logging.info(f'message {msg[ID]} processing end')
+                except Exception as err:
+                    logging.error(f"Unexpected error: {msg[ID]}: {err}")
+                    logging.info(f'message {msg[ID]} processing end')
 
-        db_utils.close_connection()
+                    db_utils.close_connection()
+        time.sleep(5 * 60)
 
 
 if __name__ == "__main__":
-    while True:
-        main()
-        time.sleep(5 * 60)
+    main()
