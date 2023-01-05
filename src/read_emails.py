7dfc23555e729c98ecd2c2a60ee067d895fe6fc3
jezinka <jezina1989@gmail.com>
2023-01-05 16:10:15 +0100

    change parsing html messages add tests

--- src/read_emails.py
+++ src/read_emails.py
@@ -2,6 +2,8 @@ import logging
 import re
 from base64 import urlsafe_b64decode
 
+from bs4 import BeautifulSoup
+
 from const import ME_ID, ID
 from message import Message
 
@@ -12,42 +14,6 @@ def search_bank_messages(service, label):
     if 'messages' in result:
         messages.extend(result['messages'])
     return messages
-
-
-def parse_message(payload):
-    message_body = ''
-    if 'data' in payload.get("body"):
-        message_body = urlsafe_b64decode(payload.get("body")['data']).decode("utf-8", "replace")
-        message_body = re.sub(r'\s{2}', '', message_body)
-    else:
-        if payload.get("parts"):
-            desired_mime_types = ['multipart/alternative', 'multipart/mixed', 'multipart/related', 'text/plain',
-                                  'text/html']
-            parts = list(filter(
-                lambda x: x.get('mimeType') in desired_mime_types, payload['parts']))
-            if len(parts) == 0:
-                logging.error(';'.join([d['mimeType'] for d in payload['parts']]))
-            for part in parts:
-                return parse_message(part)
-    return message_body
-
-
-def search_mails(service, query):
-    print(query)
-    result = service.users().messages().list(userId=ME_ID, q=query).execute()
-    if result.get('resultSizeEstimate') == 0:
-        return []
-    messages = []
-    if 'messages' in result:
-        messages.extend(result['messages'])
-
-    mails_body = []
-    for msg in messages:
-        mail = service.users().messages().get(userId=ME_ID, id=msg[ID], format='full').execute()
-        payload = mail['payload']
-        mails_body.append(parse_message(payload))
-
-    return '\n'.join(mails_body)
 
 
 def read_message(service, mail):
@@ -62,37 +28,40 @@ def process_message(payload):
 
     receive_date = list(filter(lambda x: x.get("name").lower() == "date", headers))[0].get("value")
 
-    if income:
-        m = re.search(r"Tytuł:(?P<tytul>[\w].*)Nadawca:(?P<kto>[\w].*)Kwota:(?P<kwota>[\d ]*,[\d]{2}) PLN", body)
-        if m is None:
-            m = re.search(
-                r"Ile:\+?(?P<kwota>[\d ]*,[\d]{2}) PLNKiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})(Karta:(?P<karta>[\w].*))?Gdzie:(?P<tytul>[\w].*)Telefon",
-                body)
-    else:
-        m = re.search(
-            r"(Odbiorca:(?P<kto>.*))?Ile:(?P<kwota>[\d ]*,[\d]{2}) PLNTytuł:(?P<tytul>.*)Kiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})",
-            body)
-        if m is None:
-            m = re.search(
-                r"Ile:-?(?P<kwota>[\d ]*,[\d]{2}) PLNKiedy:(?P<kiedy>[\d]{2}-[\d]{2}-[\d]{4})(Karta:(?P<karta>[\w].*))?Gdzie:(?P<tytul>.*)Telefon",
-                body)
+    message_dict = prepare_message_dict(body, income)
 
-    message = Message(m.groupdict(), income)
+    message = Message(message_dict, income)
     logging.info(message.get_title())
     message.set_receive_date(receive_date)
     return message
 
 
+def prepare_message_dict(body, income):
+    m = re.search(
+        r"Ile:[\+\-]?(?P<kwota>[\d ]*,\d{2}) PLN Kiedy:(?P<kiedy>\d{2}-\d{2}-\d{4})([\w ].*)Gdzie:(?P<tytul>\w.*)Tel",
+        body)
+
+    if m is None:
+        if income:
+            m = re.search(r"Tytuł:(?P<tytul>\w.*)Nadawca:(?P<kto>\w.*)Kwota:(?P<kwota>[\d ]*,\d{2}) PLN", body)
+        else:
+            m = re.search(
+                r"(Odbiorca:(?P<kto>.*))? Ile:(?P<kwota>[\d ]*,\d{2}) PLN Tytuł:(?P<tytul>.*) Kiedy:(?P<kiedy>\d{2}-\d{2}-\d{4})",
+                body)
+
+    return m.groupdict()
+
+
 def is_income(body, headers):
     title = list(filter(lambda x: x.get("name").lower() == "subject", headers))[0].get("value")
-    income = 'Wpływ' in title or 'Wpłata' in title
+    income = re.search('Wpływ|Wpłata', title) is not None
     if not income:
-        income = re.search(r"Ile:\+(?P<kwota>[\d ]*,[\d]{2})", body) is not None
+        income = re.search(r"Ile:\+(?P<kwota>[\d ]*,\d{2})", body) is not None
     return income
 
 
 def prepare_body(payload):
     body = urlsafe_b64decode(payload.get("body")['data']).decode("utf-8", "replace")
-    body = re.sub(r'<.*>', '', body)
-    body = re.sub(r'\s{2}', '', body)
-    return body
+    soup = BeautifulSoup(body, 'html.parser')
+    text = soup.table.table.get_text().strip()
+    return re.sub(r'\s{2}', '', text)
---
+++ test/test_read_emails.py
@@ -0,0 +1,42 @@
+import unittest
+
+from src.read_emails import prepare_message_dict
+
+
+class TestReadMails(unittest.TestCase):
+
+    def test_prepare_message_dict_empty(self):
+        with self.assertRaises(AttributeError):
+            prepare_message_dict('', True)
+
+    def test_prepare_message_dict_card_expense(self):
+        body = 'W związku z transakcją ZAKUPY na Twojej karcie debetowej zostało zablokowane -19,22 PLN. Szczegóły:Karta:Dopasowana Visa 1234 Ile:-19,22 PLN Kiedy:05-01-2023 Gdzie:JMP S.A. BIEDRONKA 1244 WROCLAWTel'
+        self.assertDictEqual(prepare_message_dict(body, False),
+                             {'kwota': '19,22', 'kiedy': '05-01-2023', 'tytul': 'JMP S.A. BIEDRONKA 1244 WROCLAW'})
+
+    def test_prepare_message_dict_income(self):
+        body = 'Na Twoje konto wpłynęło 9 999,98 PLN od PAULINA KACZMAREK.Szczegóły przelewu:Tytuł:Przelew własnyNadawca:PAULINA KACZMAREKKwota:9 999,98 PLNNa konto:11 2222 3333 4444 5555 6666 7777 Stan konta po przelewie:Saldo:9 999,99 PLN Tel'
+        self.assertDictEqual(prepare_message_dict(body, True),
+                             {'tytul': 'Przelew własny', 'kto': 'PAULINA KACZMAREK', 'kwota': '9 999,98'})
+
+    def test_prepare_message_dict_atm_income(self):
+        body = "Wpłata we wpłatomacie 100,00 PLN. Na Twoje konto wpłynęło 100,00 PLN.Szczegóły:Ile:100,00 PLN Kiedy:17-10-2022 Karta:Dopasowana Visa 1234 Gdzie:ATMTel"
+
+        self.assertDictEqual(prepare_message_dict(body, True),
+                             {'kwota': '100,00', 'kiedy': '17-10-2022', 'tytul': 'ATM'})
+
+    def test_prepare_message_dict_expense(self):
+        body = 'Stan Twojego konta zmniejszył się o 0,01 PLN. Szczegóły:Z konta:11 2222 3333 4444 5555 6666 7777 Ile:0,01 PLN Tytuł:Zakup BLIK Kiedy:03-01-2023 Data księgowania:03-01-2023Saldo po operacji: Saldo:0,02 PLN Tel'
+        self.assertDictEqual(prepare_message_dict(body, False),
+                             {'kto': None, 'kwota': '0,01',
+                              'tytul': 'Zakup BLIK',
+                              'kiedy': '03-01-2023'})
+
+    def test_prepare_message_dict_atm_expense(self):
+        body = 'Stan Twojego konta zmniejszył się o -50,00 PLN. Szczegóły:Ile:-0,01 PLN Kiedy:19-09-2022 Karta:Dopasowana Visa 1234 Gdzie:ATMTel'
+        self.assertDictEqual(prepare_message_dict(body, False),
+                             {'kwota': '0,01', 'kiedy': '19-09-2022', 'tytul': 'ATM'})
+
+
+if __name__ == '__main__':
+    unittest.main()
