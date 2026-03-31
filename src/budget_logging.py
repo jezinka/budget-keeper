c0ddae960057ad8b5d37e4bb3a960c2f73ee85ac
jezinka <jezina1989@gmail.com>
2026-03-31 17:41:36 +0200

    set logging level for pika to INFO

--- src/budget_logging.py
+++ src/budget_logging.py
@@ -8,6 +8,7 @@ from const import LOG_FILE
 def logging_config():
     file_handler = RotatingFileHandler(filename=LOG_FILE, maxBytes=1000000, backupCount=5)
     stdout_handler = logging.StreamHandler(sys.stdout)
+    logging.getLogger('pika').setLevel(logging.INFO)
     logging.basicConfig(handlers=[file_handler, stdout_handler],
                         datefmt='%Y-%m-%d %H:%M:%S',
                         format='%(asctime)s %(levelname)-8s %(message)s',
